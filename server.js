aa6fcb3133b3b7d473c5e49f5d496ba7a20b255c
matteo vona <smatteo.vona@itis.pr.it>
2023-11-29 17:30:09 +0100

    Nomi presenti in tabella utenti

--- server.js
+++ server.js
@@ -6,58 +6,65 @@ let numClienti = 0;
 const users = [];
 
 function requestHandler(request, response) {
-  fs.readFile('index.html', function (error, data) {
-    if (error) {
-      response.write(404);
-    } else {
-      response.writeHead(200, { "content-Type": "text/html" });
-      response.write(data, "utf8");
-    }
-    response.end();
-  });
+	fs.readFile('index.html', function (error, data) {
+		if (error) {
+			response.write(404);
+		} else {
+			response.writeHead(200, { "content-Type": "text/html" });
+			response.write(data, "utf8");
+		}
+		response.end();
+	});
 }
 
 const server = http.createServer(requestHandler);
 server.listen(port, ip, function () {
-  console.log("Server started on " + ip + ":" + port);
+	console.log("Server started on " + ip + ":" + port);
 });
 
 const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"]
+	}
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.username = socket.id;
-  users.push(socket.username);
-  updateConnectedUsers();
 
-  socket.emit('connesso', ip + " " + "porta:" + " " + port);
-  numClienti++;
-  io.sockets.emit('stato', numClienti);
-  console.log('Clienti connessi:', numClienti);
+	socket.emit('connesso', ip + " " + "porta:" + " " + port);
+	numClienti++;
+	io.sockets.emit('stato', numClienti);
+	console.log('Clienti connessi:', numClienti);
 
-  socket.on('messaggio', function (data) {
-    console.log("client: " + data);
-    socket.broadcast.emit('messaggio', data);
-  });
+	socket.on('messaggio', function (data) {
+		console.log("client: " + data);
+		socket.broadcast.emit('messaggio', data);
+	});
 
-  socket.on('disconnect', function () {
-    numClienti--;
-    console.log('Clienti connessi:', numClienti);
-    io.sockets.emit('stato', numClienti);
-    console.log('utente: disconnesso ' + socket.username);
+	socket.on('utentissimo', function (nickname) {
+		//socket.nome = nickname;
+		//console.log(nickname);
+		//socket.id è un id univoco creato automaticamente da socket.io all'avvio di una connessione
+		//socket.username = socket.id;
+		socket.username = nickname;
+		users.push(socket.username);
+		updateConnectedUsers();
+	});
 
-    const index = users.indexOf(socket.username);
-    if (index > -1) {
-      users.splice(index, 1);
-    }
-    updateConnectedUsers();
-  });
+	socket.on('disconnect', function () {
+		numClienti--;
+		console.log('Clienti connessi:', numClienti);
+		io.sockets.emit('stato', numClienti);
+		console.log('utente: disconnesso ' + socket.username);
+
+		const index = users.indexOf(socket.username);
+		if (index > -1) {
+			users.splice(index, 1);
+		}
+		updateConnectedUsers();
+	});
 });
 
 function updateConnectedUsers() {
-  io.sockets.emit('update-users', users);
+	io.sockets.emit('update-users', users);
 }
